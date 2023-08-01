3dee27ddc1ff5d5f5ed8fa9f34f6492b0cb46734
wanickols01 <wanickols@ualr.edu>
2023-08-01 00:34:35 -0500

    Cutscene 1
    Created a basic idea for cutscenes, created an interface to have commands, and then created a basic move character command to test. Cutscenes use the cutscene manager to play and stop, and can be set to autoplay and be destroyed on finishing.

--- Assets/Scripts/Core/Utility/Direction.cs
+++ Assets/Scripts/Core/Utility/Direction.cs
@@ -27,5 +27,7 @@ namespace Core
         public static readonly Vector2Int Down = new Vector2Int(0, -1);
         public static readonly Vector2Int Left = new Vector2Int(-1, 0);
         public static readonly Vector2Int Right = new Vector2Int(1, 0);
+
+
     }
 }
