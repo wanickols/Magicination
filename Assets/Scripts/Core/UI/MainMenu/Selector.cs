de6f9a17d08b2ac3085636a298d7f3d74c75f7ad
wanickols01 <wanickols@ualr.edu>
2023-08-12 20:06:32 -0500

    Menu Update 1
    Updated Menu. Created an equipment Menu.
    Created a Selector Class that is capable of moving through items in a list.
    Updated MainMenu Script to use selectors to handle input on menu.
    Menu now handles all it's own input, not inputhandler.

---
+++ Assets/Scripts/Core/UI/MainMenu/Selector.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class MenuSelector : MonoBehaviour
+    {
+        private MainMenu mainMenu;
+        private RectTransform rectTransform;
+        private List<RectTransform> selectableOptions = new List<RectTransform>();
+
+        public event Action SelectionChanged;
+
+        private int _selectedIndex = 0;
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set
+            {
+                _selectedIndex = value;
+                SelectionChanged?.Invoke();
+            }
+        }
+
+        public IReadOnlyList<RectTransform> SelectableOptions => selectableOptions;
+
+        private void Awake()
+        {
+            mainMenu = GetComponentInParent<MainMenu>();
+            rectTransform = GetComponent<RectTransform>();
+
+            for (int i = 0; i < rectTransform.parent.childCount; i++)
+            {
+                if (rectTransform.parent.GetChild(i).CompareTag("Selectable"))
+                    selectableOptions.Add(rectTransform.parent.GetChild(i).GetComponent<RectTransform>());
+            }
+        }
+
+
+        void Update()
+        {
+            if (mainMenu.CurrentSelector != this)
+                return;
+
+            if (rectTransform.anchoredPosition != selectableOptions[SelectedIndex].anchoredPosition)
+                MoveToSelectedOption();
+        }
+
+        private void MoveToSelectedOption()
+        {
+            rectTransform.anchoredPosition = Vector2.MoveTowards(rectTransform.anchoredPosition, selectableOptions[SelectedIndex].anchoredPosition, 8f);
+        }
+    }
+}
